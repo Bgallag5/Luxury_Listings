8b4f01fe30a3c3d4dfa8029ff42834a09a8eb78f
bengallagher <bgallag5@gmail.com>
2022-05-18 14:04:17 -0400

    calendar date select

--- src/components/HouseAbout/HouseAbout.js
+++ src/components/HouseAbout/HouseAbout.js
@@ -1,6 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Indicator } from "@mantine/core";
+import { Calendar } from "@mantine/dates";
 
 export default function HouseAbout() {
+  const [calendarValue, setCalendarValue] = useState(null);
+
+  const handleCalendarChange = (e) => {
+    //handle time select popup
+    //save time and date as data (to be confirmed in email? or somehting)
+  }
+
+  const BookingCalendar = () => {
+    return (
+      <Calendar
+        renderDay={(date) => {
+          //indicator on todays date
+          const day = date.getDate();
+          const today = new Date(Date.now()).getDate();
+          return (
+            <Indicator size={6} color="red" offset={8} disabled={day !== today}>
+              <div>{day}</div>
+            </Indicator>
+          );
+        }}
+        value={calendarValue}
+        onChange={setCalendarValue}
+        onClick={(e) => handleCalendarChange(e)}
+        firstDayOfWeek="sunday"
+      />
+    );
+  };
+
+  console.log(calendarValue);
   return (
     <div className="about-section">
       <div className="info__description">
@@ -17,6 +48,9 @@ export default function HouseAbout() {
           Place, shops & restaurants, Anaeho’omalu bay with white sandy beach
           and snorkeling, and Waikoloa golf course nearby.
         </p>
+      </div>
+      <div className="booking">
+        <BookingCalendar />
       </div>
     </div>
   );
